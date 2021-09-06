bd304653c23e7dcdc281847913579c2ba4f95897
Renchester Ramos <renchesterjramos@gmail.com>
2021-09-06 15:18:25 +0800

    Add query view functions

---
+++ src/views/queryView.js
@@ -0,0 +1,18 @@
+export const queryView = (function () {
+  const form = document.querySelector('.weather-form');
+  const queryEl = document.querySelector('.weather-query');
+
+  const addHandlerGetQuery = function (handler) {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      const query = queryEl.value;
+
+      handler(query);
+    });
+  };
+
+  return {
+    addHandlerGetQuery,
+  };
+})();
