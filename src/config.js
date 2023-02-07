abf86e4b2e7d9b31dda4c2dcd834953dc848f000
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-07 08:57:31 +0800

    Initial commit on new branch

--- src/config.js
+++
@@ -1,7 +0,0 @@
-export const WEATHER_API_KEY = `9f5a0a9571f86000054119d65c0d4f86`;
-
-export const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5`;
-
-export const LOC_API_KEY = `pk.c204d4527b9ac16a572295302cc0c574`;
-
-export const LOC_API_URL = `https://us1.locationiq.com/v1/reverse.php`;
