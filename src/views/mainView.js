3810b416457759050749e488b4c9e0340d8e9e2c
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-08 19:32:49 +0800

    Separate box and main view into own modules

---
+++ src/views/mainView.js
@@ -0,0 +1,33 @@
+import View from './View';
+
+const MainView = (() => {
+  const renderWeatherMain = (data) => {
+    const containerEl = document.querySelector('.weather--main');
+    const footerEl = document.querySelector('.weather--footer');
+
+    const locationEl = document.querySelector('.main--location');
+    const iconEl = document.querySelector('.main--icon');
+    const tempEl = document.querySelector('.temp-value');
+    const descEl = document.querySelector('.main--desc');
+    const tempMaxEl = document.querySelector('.temp-extreme--high .temp-value');
+    const tempMinEl = document.querySelector('.temp-extreme--low .temp-value');
+    const footerLocEl = document.querySelector('.weather-footer--location');
+
+    locationEl.textContent = data.location;
+    tempEl.textContent = +data.temp.toFixed(1);
+    descEl.textContent = data.description;
+    tempMaxEl.textContent = +data.temp_max.toFixed(1);
+    tempMinEl.textContent = +data.temp_min.toFixed(1);
+    footerLocEl.textContent = data.location;
+    iconEl.classList.add(`wi-owm-${data.timeOfDay}-${data.id}`);
+
+    View.unhideEl(containerEl);
+    View.unhideEl(footerEl);
+  };
+
+  return {
+    renderWeatherMain,
+  };
+})();
+
+export default MainView;
