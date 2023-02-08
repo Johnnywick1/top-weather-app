b59dca14ccdea6498cb406dadf7a0334846b2d67
Renchester Ramos <renchesterjramos@gmail.com>
2023-02-08 14:07:02 +0800

    Update config files

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_KEY = `b8d66267ba5b216933d4adef14325a11`;
+
+export const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/`;
+
+export const GEO_API_URL = `http://api.openweathermap.org/geo/1.0`;
